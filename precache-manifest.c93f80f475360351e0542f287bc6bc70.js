24d48159e7f6f788ec3ccb8883ec9ce898177759
Kaka <potatokaka@gmail.com>
2019-09-16 23:43:35 +0800

    Updates

---
+++ precache-manifest.c93f80f475360351e0542f287bc6bc70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd4adc77c52e78bfc1018fd93b58624",
+    "url": "/gomoku_react/index.html"
+  },
+  {
+    "revision": "f1c0b9740685494b2cf0",
+    "url": "/gomoku_react/static/css/main.4095680a.chunk.css"
+  },
+  {
+    "revision": "0766bb5edaa9ef7f770d",
+    "url": "/gomoku_react/static/js/2.16ae0c99.chunk.js"
+  },
+  {
+    "revision": "f1c0b9740685494b2cf0",
+    "url": "/gomoku_react/static/js/main.6846f1b6.chunk.js"
+  },
+  {
+    "revision": "13018a076cea02b8d7df",
+    "url": "/gomoku_react/static/js/runtime~main.38a3ef93.js"
+  }
+]);
